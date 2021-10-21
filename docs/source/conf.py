2833b3f44c0c3bee60f9e1398017542ee02f54d1
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2021-10-21 16:16:17 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Mygeoip'
+copyright = '2021, Deadshot0x7'
+author = 'SayyedViquarAhmed (Deadshot0x7)'
 
 release = '0.1'
 version = '0.1.0'
